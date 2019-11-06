ede369eb2c19ee4175ab2bb35bf085879072d72d
Jacob Smith <jsmithdenverdev@gmail.com>
2019-11-06 08:40:46 -0700

    Created a solution, a BusinessLogic assembly and a Functions assembly

---
+++ HelloAzure.BusinessLogic/GreetingService.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloAzure.BusinessLogic
+{
+    public class GreetingService
+    {
+        public string GetGreeting(string name)
+        {
+            return $"Hello, {name}";
+        }
+    }
+}
